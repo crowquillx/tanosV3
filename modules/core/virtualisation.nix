91109b99b75d7b048e680f452e46cb10f4afe343
tan <tancodes@proton.me>
2025-04-21 19:03:10 -0600

    add podman and distrobox

--- modules/core/virtualisation.nix
+++ modules/core/virtualisation.nix
@@ -2,13 +2,16 @@
   # Only enable either docker or podman -- Not both
   virtualisation = {
     libvirtd.enable = false;
-    docker.enable = true;
-    podman.enable = false;
+    docker.enable = false;
+    podman.enable = true; 
+    podman.dockerCompat = true;
+    podman.defaultNetwork.settings.dns_enabled = true;
   };
   programs = {
     virt-manager.enable = false;
   };
   environment.systemPackages = with pkgs; [
     # virt-viewer # View Virtual Machines
+    distrobox
   ];
 }
