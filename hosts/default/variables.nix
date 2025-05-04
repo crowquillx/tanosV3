51615d9730af38ae0352876f7b6f9ebaf3bb6229
tan <tancodes@proton.me>
2025-05-04 14:05:22 -0600

    cleanup

--- hosts/default/variables.nix
+++ hosts/default/variables.nix
@@ -1,16 +1,20 @@
 {
   # Git Configuration ( For Pulling Software Repos )
-  gitUsername = "Tyler Kelley";
-  gitEmail = "tylerzanekelley@gmail.com";
+  gitUsername = "tan";
+  gitEmail = "tancodes@proton.me";
 
   # Hyprland Settings
-  extraMonitorSettings = "";
+  extraMonitorSettings = "
+monitor=DP-1,2560x1440@165,2560x1080,auto,vrr,1
+monitor=DP-2,2560x1440@144,0x1080,auto
+monitor=DP-3,1920x1080@144,2560x0,auto
+monitor=HDMI-A-1,disabled";
 
   # Waybar Settings
   clock24h = false;
 
   # Program Options
-  browser = "brave"; # Set Default Browser (google-chrome-stable for google-chrome)
+  browser = "zen-beta"; # Set Default Browser (google-chrome-stable for google-chrome)
   terminal = "kitty"; # Set Default System Terminal
   keyboardLayout = "us";
   consoleKeyMap = "us";
@@ -26,19 +30,51 @@
   printEnable = false;
 
   # Set Stylix Image
-  stylixImage = ../../wallpapers/AnimeGirlNightSky.jpg;
-
-  # Set Waybar
-  # Includes alternates such as waybar-curved.nix & waybar-ddubs.nix
-  waybarChoice = ../../modules/home/waybar/waybar-simple.nix;
+  stylixImage = ../../wallpapers/wp.png;
 
   # Set Animation style
   # Available options are:
   # animations-def.nix  (default)
   # animations-end4.nix (end-4 project)
   # animations-dynamic.nix (ml4w project)
-  animChoice = ../../modules/home/hyprland/animations-def.nix;
+  animChoice = ../../modules/home/hyprland/animations-end4.nix;
 
   # Enable Thunar GUI File Manager
-  thunarEnable = false;
+  thunarEnable = true;
+
+  # Enable Rofi
+  enableRofi = true;
+
+  # Core Module Enables
+  flatpakEnable = true;
+  greetdEnable = true;
+  steamEnable = true;
+  stylixEnable = true;
+  syncthingEnable = false;
+  xserverEnable = false;
+
+  # Service Enables
+  libinputEnable = true;
+  fstrimEnable = true;
+  gvfsEnable = true;
+  opensshEnable = true;
+  bluemanEnable = true;
+  tumblerEnable = true;
+  gnomeKeyringEnable = true;
+  smartdEnable = true;
+  pipewireEnable = true;
+
+  ghosttyEnable = true;
+  wlogoutEnable = true;
+  hyprlandEnable = true;
+  hyprlockEnable = true;
+  hypridleEnable = true;
+  nvfEnable = true;
+  weztermEnable = false;
+  kittyEnable = true;
+  zoxideEnable = true;
+  waybarEnable = true;
+
+  # Set Waybar
+  waybarChoice = "waybar-curved";
 }
